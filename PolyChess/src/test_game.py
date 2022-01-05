3124739b8409b7629983796dc5e1e565d5dcee15
unknown <thomas26.nicolas@gmail.com>
2022-01-05 16:21:01 +0100

    Rajout test_game

---
+++ PolyChess/src/test_game.py
@@ -0,0 +1,51 @@
+import chess
+
+board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 4")
+print(board)
+
+def startGame():
+
+    isGameOn = True
+    white = True
+
+    while isGameOn:
+        
+        coup = input('Jouez un coup \n')
+        
+        if white == True:
+            
+            while isLegal(coup) == False :            
+                coup = input('Jouez un coup \n')
+                
+            board.push(chess.Move.from_uci(coup))
+            white = False
+       
+        else :
+            
+            while isLegal(coup) != True : 
+                coup = input('Jouez un coup \n')
+            board.push(chess.Move.from_uci(coup))
+            white = True
+            
+        print(board)
+        
+        
+def isLegal(coup) :
+      
+   try:
+       move = chess.Move.from_uci(coup)
+              
+       if move in board.legal_moves:
+           return True
+       else:
+           print("Coup Illégal !")
+           return False
+       
+   except ValueError:
+       print("Coup Illégal !")
+       return False
+           
+        
+
+startGame()
+
