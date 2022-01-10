b3b962912a849b57a3afcb6912ff19ea7531b310
unknown <wiwizekill@gmail.com>
2022-01-10 11:45:37 +0100

    search

--- PolyChess/src/engine.py
+++ PolyChess/src/engine.py
@@ -1,2 +1,23 @@
+import chess
+
 class Engine:
-    pass
+    def __init__(self):
+        self.num_positions = 0
+
+    def search(self, board, depth):
+
+        if depth == 0:
+            return 1
+
+        for move in board.legal_moves:
+            board.push(move)
+            self.search(board, depth - 1)
+
+            #update num_positions
+            self.num_positions += 1
+            if depth > 1:
+                self.num_positions -= 1
+            board.pop()
+
+    def evaluate(self, board):
+        pass
